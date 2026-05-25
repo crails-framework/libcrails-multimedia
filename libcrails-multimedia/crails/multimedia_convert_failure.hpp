de897e6907c75749d2c9db551f802795d4de5e17
plaristote <michael@unetresgrossebite.com>
2026-05-25 07:03:17 +0200

    adds missing includes in MultimediaConvertFailure header

--- libcrails-multimedia/crails/multimedia_convert_failure.hpp
+++ libcrails-multimedia/crails/multimedia_convert_failure.hpp
@@ -1,6 +1,9 @@
 #ifndef  CRAILS_MULTIMEDIA_CONVERT_FAILURE_HPP
 # define CRAILS_MULTIMEDIA_CONVERT_FAILURE_HPP
 
+# include <string>
+# include <crails/utils/backtrace.hpp>
+
 namespace Crails
 {
   struct MultimediaConvertFailure : public boost_ext::runtime_error
